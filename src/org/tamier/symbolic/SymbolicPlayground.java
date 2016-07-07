87f2a9ee32240a4af98cd57e852c5187ed82d5fb
tamier <m2ta@uwaterloo.ca>
2016-07-07 13:40:38 -0400

    Remove SymbolicTypeProcessor
    Doesn't depend on JavaSource2CFGDOT anymore.
    Adapt to the new change of DataflowTypeProcessor

--- src/org/tamier/symbolic/SymbolicPlayground.java
+++ src/org/tamier/symbolic/SymbolicPlayground.java
@@ -1,19 +1,48 @@
 package org.tamier.symbolic;
-import org.checkerframework.dataflow.cfg.JavaSource2CFGDOT;
+
+import org.checkerframework.dataflow.analysis.Analysis;
+import org.checkerframework.dataflow.cfg.DataflowTypeProcessor;
+
+import java.util.Set;
+
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+
+import com.sun.tools.javac.file.JavacFileManager;
+import com.sun.tools.javac.main.JavaCompiler;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.List;
 
 public class SymbolicPlayground {
     /* Configuration: change as appropriate */
     // input file name and path
     static String inputFile = "//home/tamier/jsr308/SymbolicExecution/src/test/SymbolicTest3.java";
-    static String outputDir = "/home/tamier"; // output directory
-    static String method = "foo"; // name of the method to analyze
     public static String clazz = "test.SymbolicTest3"; // name of the class to
-                                                  // consider
+                                                // consider
+    static String method = "foo"; // name of the method to analyze
 
     public static void main(String[] args) {
 
-        SymbolicTypeProcessor typeProcessor = new SymbolicTypeProcessor();
+        Context context = new Context();
+        JavaCompiler javac = new JavaCompiler(context);
+        javac.attrParseOnly = true;
+        JavacFileManager fileManager = (JavacFileManager) context.get(JavaFileManager.class);
+        JavaFileObject fileObject = fileManager
+                .getJavaFileObjectsFromStrings(List.of(inputFile)).iterator().next();
 
-        JavaSource2CFGDOT.launch(inputFile, outputDir, method, clazz, true, typeProcessor);
+        DataflowTypeProcessor typeProcessor = new DataflowTypeProcessor() {};
+        typeProcessor.setMethodNameToProcess(method);
+        javac.compile(List.of(fileObject), List.of(clazz),
+                List.of(typeProcessor));
+
+        SymbolicTransfer transfer = new SymbolicTransfer();
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        Analysis analysis = new Analysis(null, transfer);
+        analysis.performAnalysis(typeProcessor.getCFG());
+        Set<Set<Constraint>> permutatedConstraintSet = transfer
+                .getConstrainManager().getPermutatedSets();
+        SymbolicSolver solver = new SymbolicSolver();
+        solver.solvePermutatedConstraintSet(permutatedConstraintSet);
+
     }
 }
